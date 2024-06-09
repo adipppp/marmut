3d2c4ebdc801eb6fc260fba1a2d7d32cb5809cb6
adipppp <adhif2004@gmail.com>
2024-06-09 14:26:05 +0700

    feat: Add actual music playback to play command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -2,6 +2,8 @@ import {
     ChatInputCommandInteraction,
     GuildMember,
     SlashCommandBuilder,
+    SlashCommandOptionsOnlyBuilder,
+    VoiceBasedChannel,
 } from "discord.js";
 import { Command } from "../../types";
 import {
@@ -10,47 +12,27 @@ import {
     memberInVoiceChannel,
 } from "../../utils/functions";
 import { joinVoiceChannel } from "@discordjs/voice";
+import YouTube from "youtube-sr";
+import { Song, musicPlayers } from "../../core/music";
 
 export class PlayCommand implements Command {
-    readonly data: SlashCommandBuilder;
+    readonly data: SlashCommandOptionsOnlyBuilder;
 
     constructor() {
         this.data = new SlashCommandBuilder()
             .setName("play")
-            .setDescription("Play a song");
+            .setDescription("Play a song")
+            .addStringOption((builder) =>
+                builder
+                    .setName("query")
+                    .setDescription("The song to play. Can also be a URL.")
+                    .setRequired(true)
+            );
     }
 
-    private async joinVoiceChannel(interaction: ChatInputCommandInteraction) {
-        const guild = interaction.guild!;
-        const member = interaction.member!;
-
-        if (!(member instanceof GuildMember)) {
-            throw new Error("member is not a GuildMember");
-        }
-
-        if (!memberInSameVoiceChannel(member) && !clientIsPlayingAudio(guild)) {
-            await interaction.reply({
-                content:
-                    "Bot is already playing music in another voice channel.",
-                ephemeral: true,
-            });
-            return false;
-        }
-
-        const voiceState = member.voice;
-        const channelId = voiceState.channelId!;
-        const guildId = guild.id;
-
-        joinVoiceChannel({
-            channelId,
-            guildId,
-            adapterCreator: guild.voiceAdapterCreator,
-        });
-
-        return true;
-    }
-
-    async run(interaction: ChatInputCommandInteraction) {
+    private async validatePreconditions(
+        interaction: ChatInputCommandInteraction
+    ) {
         const guild = interaction.guild!;
         const member = guild.members.cache.get(interaction.user.id)!;
 
@@ -60,15 +42,67 @@ export class PlayCommand implements Command {
                     "You need to be in a voice channel to use this command.",
                 ephemeral: true,
             });
+            return false;
+        }
+
+        if (!memberInSameVoiceChannel(member) && !clientIsPlayingAudio(guild)) {
+            await interaction.reply({
+                content:
+                    "Bot is already playing a song in another voice channel.",
+                ephemeral: true,
+            });
+            return false;
+        }
+
+        const channelId = member.voice.channelId!;
+        const voiceChannel = guild.channels.cache.get(
+            channelId
+        ) as VoiceBasedChannel;
+
+        if (!voiceChannel.joinable) {
+            await interaction.reply({
+                content: "Unable to connect to the voice channel.",
+                ephemeral: true,
+            });
+            return false;
+        }
+
+        return true;
+    }
+
+    async run(interaction: ChatInputCommandInteraction) {
+        if (!(await this.validatePreconditions(interaction))) {
             return;
         }
 
-        if (!(await this.joinVoiceChannel(interaction))) {
+        await interaction.deferReply();
+
+        const member = interaction.member as GuildMember;
+        const guild = interaction.guild!;
+        const channelId = member.voice.channelId!;
+        const guildId = guild.id;
+        const adapterCreator = guild.voiceAdapterCreator;
+
+        joinVoiceChannel({ guildId, channelId, adapterCreator });
+
+        const query = interaction.options.getString("query")!;
+        const result = await YouTube.searchOne(query);
+
+        if (!result) {
+            await interaction.editReply("Cannot find the song.");
             return;
         }
 
-        // TODO: play song
+        const song = new Song({
+            title: result.title!,
+            thumbnailUrl: result.thumbnail!.url!,
+            videoUrl: result.url,
+            duration: result.duration!,
+        });
 
-        await interaction.reply("Playing a song...");
+        const player = musicPlayers.get(guildId);
+        await player.play(song);
+
+        await interaction.editReply("Playing a song...");
     }
 }
