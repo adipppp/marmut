413226f80bdd8b5923765e6b9f15f0be8afcb3fd
adipppp <adhif2004@gmail.com>
2024-06-18 19:34:44 +0700

    feat: Implement "join" command for connection to voice channels

---
+++ src/commands/music/join.ts
@@ -0,0 +1,132 @@
+import {
+    ChatInputCommandInteraction,
+    GuildMember,
+    SlashCommandBuilder,
+    SlashCommandOptionsOnlyBuilder,
+    VoiceBasedChannel,
+} from "discord.js";
+import { Command } from "../../types";
+import {
+    clientInSameVoiceChannelAs,
+    clientIsPlayingIn,
+    inVoiceChannel,
+} from "../../utils/functions";
+import { joinVoiceChannel } from "@discordjs/voice";
+
+export class JoinCommand implements Command {
+    readonly data: SlashCommandOptionsOnlyBuilder;
+
+    constructor() {
+        this.data = new SlashCommandBuilder()
+            .setName("join")
+            .setDescription("Connects to a voice channel.")
+            .addChannelOption((builder) =>
+                builder
+                    .setName("channel")
+                    .setDescription(
+                        "The voice channel to connect to. Defaults to your current voice channel."
+                    )
+                    .setRequired(false)
+            )
+            .setDMPermission(false);
+    }
+
+    private async validateArgs(interaction: ChatInputCommandInteraction) {
+        const channel = interaction.options.getChannel("channel");
+        const guild = interaction.guild!;
+
+        if (channel !== null) {
+            const channelId = channel.id;
+            const channelFromCache = guild.channels.cache.get(channelId)!;
+
+            if (!channelFromCache.isVoiceBased()) {
+                await interaction.reply({
+                    content: "The channel specified is not a voice channel.",
+                    ephemeral: true,
+                });
+                return false;
+            }
+        } else {
+            const member = interaction.member as GuildMember;
+
+            if (!inVoiceChannel(member)) {
+                await interaction.reply({
+                    content:
+                        "You need to specify or be in a voice channel to use this command.",
+                    ephemeral: true,
+                });
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private async validatePreconditions(
+        interaction: ChatInputCommandInteraction
+    ) {
+        if (!(await this.validateArgs(interaction))) {
+            return false;
+        }
+
+        const guild = interaction.guild!;
+        const member = interaction.member as GuildMember;
+
+        if (!clientInSameVoiceChannelAs(member) && clientIsPlayingIn(guild)) {
+            await interaction.reply({
+                content:
+                    "Bot is already playing a song in another voice channel.",
+                ephemeral: true,
+            });
+            return false;
+        }
+
+        const channelId = member.voice.channelId!;
+        const voiceChannel = guild.channels.cache.get(
+            channelId
+        ) as VoiceBasedChannel;
+
+        if (!voiceChannel.joinable) {
+            await interaction.reply({
+                content: "Unable to connect to the voice channel.",
+                ephemeral: true,
+            });
+            return false;
+        }
+
+        return true;
+    }
+
+    async run(interaction: ChatInputCommandInteraction) {
+        if (!(await this.validatePreconditions(interaction))) {
+            return;
+        }
+
+        const guild = interaction.guild!;
+        const guildId = guild.id;
+
+        const clientId = interaction.client.user.id;
+        const clientVoiceState = guild.voiceStates.cache.get(clientId);
+        const clientVoiceChannelId = clientVoiceState?.channelId;
+
+        const member = interaction.member as GuildMember;
+        const memberVoiceChannel = member.voice.channel!;
+
+        const channel = interaction.options.getChannel("channel");
+        const channelId = (channel ?? memberVoiceChannel).id;
+
+        if (clientVoiceChannelId === channelId) {
+            await interaction.reply({
+                content: "Already connected to the voice channel.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        const adapterCreator = guild.voiceAdapterCreator;
+
+        joinVoiceChannel({ guildId, channelId, adapterCreator });
+
+        await interaction.reply("Connected to the voice channel.");
+    }
+}
