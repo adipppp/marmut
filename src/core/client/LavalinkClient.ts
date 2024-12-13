680c820ce4ecce0ef4a1fc5cf78dbe51399aae6a
adipppp <adhif2004@gmail.com>
2024-12-13 11:16:24 +0700

    feat: Add support for secure Lavalink nodes

--- src/core/client/LavalinkClient.ts
+++ src/core/client/LavalinkClient.ts
@@ -4,6 +4,8 @@ import { marmut } from "./MarmutClient";
 const nodeName = process.env.LAVALINK_NODE_NAME;
 const nodeUrl = process.env.LAVALINK_NODE_URL;
 const nodeAuth = process.env.LAVALINK_NODE_AUTH;
+const nodeIsSecure =
+    process.env.LAVALINK_NODE_IS_SECURE?.toLowerCase() === "true";
 
 if (nodeName === undefined) {
     throw new Error("LAVALINK_NODE_NAME environment variable is undefined.");
@@ -15,7 +17,9 @@ if (nodeAuth === undefined) {
     throw new Error("LAVALINK_NODE_AUTH environment variable is undefined.");
 }
 
-const nodes = [{ name: nodeName, url: nodeUrl, auth: nodeAuth }];
+const nodes = [
+    { name: nodeName, url: nodeUrl, auth: nodeAuth, secure: nodeIsSecure },
+];
 
 export const lavalinkClient = new Shoukaku(
     new Connectors.DiscordJS(marmut),
